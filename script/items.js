7a3cbe0f3449fbeb33fa934c18fdaed7d8934e2a
Aharon Turpie <aaturpie@verizon.net>
2017-05-29 15:23:39 -0400

    Initial commit

---
+++ script/items.js
@@ -0,0 +1,172 @@
+var defaultItemGrid = [
+    [
+        "bow",
+        "hookshot",
+        "hammer",
+        "firerod",
+        "glove",
+        "moonpearl",
+    ],
+    [
+        "somaria",
+        "lantern",
+        "flute",
+        "book",
+        "boots",
+        "flippers",
+        "mirror",
+    ],
+    [
+        "sword",
+        "tunic",
+        "shield",
+        "blank",
+        "bombos",
+        "ether",
+        "quake",
+    ],
+        [
+        "shovel",
+        "mushroom",
+        "powder",
+        "bottle",
+        "cape",
+        "icerod",
+    ],
+    [
+        "boss3",
+        "boss4",
+        "boss5",
+        "boss6",
+        "boss7",
+        "boss8",
+        "boss9",
+    ],
+    [
+        "blank",
+        "boss0",
+        "boss1",
+        "boss2",
+        "agahnim",
+        "blank",
+    ],
+]
+
+var items = {
+    bow: 0,
+    hookshot: false,
+    hammer: false,
+    firerod: false,
+    glove: 0,
+    moonpearl: false,
+    sword: 1,
+    tunic: 1,
+    shield: 0,
+    bombos: false,
+    ether: false,
+    quake: false,
+    somaria: false,
+    lantern: false,
+    flute: false,
+    book: false,
+    boots: false,
+    flippers: false,
+    mirror: false,
+    shovel: false,
+    mushroom: false,
+    powder: false,
+    bottle:0,
+    cape: false,
+    icerod: false,
+    byrna: false,
+    net: false,
+
+    boss0: 1,
+    boss1: 1,
+    boss2: 1,
+    boss3: 1,
+    boss4: 1,
+    boss5: 1,
+    boss6: 1,
+    boss7: 1,
+    boss8: 1,
+    boss9: 1,
+    agahnim: 0,
+
+    blank: false
+};
+
+var dungeonchests = {
+    0: 3,
+    1: 2,
+    2: 2,
+    3: 5,
+    4: 6,
+    5: 2,
+    6: 4,
+    7: 3,
+    8: 2,
+    9: 5     
+}
+
+
+var itemsMin = {
+    sword:0,
+    shield:0,
+    tunic:1,
+
+    bottle:0,
+    bow:0,
+    boomerang:0,
+    glove:0,
+
+	boss0: 1,
+	boss1: 1,
+	boss2: 1,
+
+    agahnim:0,
+
+	boss3: 1,
+	boss4: 1,
+	boss5: 1,
+	boss6: 1,
+	boss7: 1,
+	boss8: 1,
+	boss9: 1
+};
+
+var itemsMax = {
+    sword:4,
+    shield:3,
+    tunic:3,
+
+    bottle:4,
+    bow:3,
+    boomerang:3,
+    glove:2,
+
+	boss0: 2,
+	boss1: 2,
+	boss2: 2,
+
+    agahnim:1,
+
+	boss3: 2,
+	boss4: 2,
+	boss5: 2,
+	boss6: 2,
+	boss7: 2,
+	boss8: 2,
+	boss9: 2,
+	
+	chest0: 3,
+    chest1: 2,
+    chest2: 2,
+	chest3: 5,
+    chest4: 6,
+    chest5: 2,
+    chest6: 4,
+    chest7: 3,
+    chest8: 2,
+    chest9: 5 
+};
